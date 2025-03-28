1bc209c462c0a2ceca88aea54b619093abeb1fbd
Gabryel-777 <gabryeldasilvalemos@gmail.com>
2025-03-28 19:30:52 -0300

    cebolinha trocando letras

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string cebola;
+Console.Clear();
+Console.Write("Escreva uma frase que você goste (de preferencia uma frase que contenha R, r.) :");
+
+cebola = Console.ReadLine()!
+.Replace("r","l")
+.Replace("R","L");
+Console.Write("ANALISANDO");
+Thread.Sleep(1500);
+Console.Write(" .");
+Thread.Sleep(1500);
+Console.Write(".");
+Thread.Sleep(1500);
+Console.Write(".");
+Console.WriteLine("");
+Console.WriteLine($"Cebolinha repetiu sua frase : {cebola}");
